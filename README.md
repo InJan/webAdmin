d9c03e70cdf9b31aebba8c34b7e58a6b9accf8bc
injan <514173569@qq.com>
2019-08-09 14:02:35 +0800

    fix the bug of annotion mistake

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ $ npm start
 
 (测试需要)
 现已关闭Koa-logger
+
+pagination.js 的使用需要在页面js 中写goPage(pageNum)//pageNum开始显示页码(首发)
--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -8,9 +8,9 @@
 function goPage(pno){
     var itable = document.getElementById("idData");
     var num = itable.rows.length;//表格所有行数(所有记录数)
-    console.log("总行数"+num);
+    // console.log("总行数"+num);
     var totalPage = 0;//总页数
-    // var pageSize = 7;//每页显示行数
+    var pageSize = 7;//每页显示行数
     //总共分几页
     if(num/pageSize > parseInt(num/pageSize)){
         totalPage=parseInt(num/pageSize)+1;
